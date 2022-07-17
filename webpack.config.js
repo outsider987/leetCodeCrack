d7a6fcf04cf53798d4996809b3e60b7672f4838c
outsider987 <t7902195204@gmail.com>
2022-07-17 22:39:42 +0800

    fix: fix animate problem

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,12 @@ module.exports = {
         use: [
           MiniCssExtractPlugin.loader,
           'css-loader',
-          'postcss-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: [require('autoprefixer')],
+            },
+          },
           'sass-loader',
         ],
       },
@@ -79,12 +84,12 @@ module.exports = {
 
   devServer: {
     static: {
-        directory: path.resolve(__dirname, './dist')
-      },
-      allowedHosts: 'all',
+      directory: path.resolve(__dirname, './dist'),
+    },
+    allowedHosts: 'all',
     compress: true,
     hot: true,
-    historyApiFallback: { index: "/", disableDotRule: true },
+    historyApiFallback: { index: '/', disableDotRule: true },
     // static: path.join(__dirname, 'dist'),
     open: true,
     port: 8080,
