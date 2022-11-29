c8c1f07559b00c7280ed01eaa5bba76f6bf73ccb
Victor <t7902195204@gmail.com>
2022-11-29 23:11:38 +0800

    fix: add carousel timer for next image

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 // images must be an array of urls , if using Next JS this could something like
 // const images = ['/img/img1.png', '/img/img2.png', '/img/img3.png']
@@ -11,45 +11,69 @@ interface Props extends React.HTMLAttributes<HTMLDivElement> {
 }
 const Carousel = (props: Props) => {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const currentIndexRef = useRef(0);
+  const isHoverImage = useRef(false);
   const ZindexRef = useRef('z-[3]');
 
   const onNext = () => {
-    const nextIndex = currentIndex === props.images.length - 1 ? 0 : currentIndex + 1;
-    setCurrentIndex(nextIndex);
+    const nextIndex = currentIndexRef.current === props.images.length - 1 ? 0 : currentIndexRef.current + 1;
+    currentIndexRef.current = nextIndex;
+    setCurrentIndex(currentIndexRef.current);
     ZindexRef.current = 'z-[3]';
   };
   const onPrev = () => {
-    const nextIndex = currentIndex === 0 ? props.images.length - 1 : currentIndex - 1;
-    setCurrentIndex(nextIndex);
+    const nextIndex = currentIndexRef.current === 0 ? props.images.length - 1 : currentIndexRef.current - 1;
+    currentIndexRef.current = nextIndex;
+    setCurrentIndex(currentIndexRef.current);
     ZindexRef.current = 'z-[0]';
   };
   const onAimatedCondition = (index: number) => {
-    if (index === currentIndex) return 'transform  translate-x-0 z-[2]';
-
-    if (index === currentIndex + 1 || (props.images.length - 1 === currentIndex && index === 0))
+    if (index === currentIndexRef.current) return 'transform  translate-x-0 z-[2]';
+    if (index === currentIndexRef.current + 1 || (props.images.length - 1 === currentIndexRef.current && index === 0))
       return 'transform  translate-x-full z-[1] ';
-
     return `transform  -translate-x-full ${ZindexRef.current}`;
   };
+  useEffect(() => {
+    const interval = setInterval(() => {
+      !isHoverImage.current && onNext();
+    }, 2800);
+    return () => clearInterval(interval);
+  }, []);
 
   return (
-    <div className="relative min-h-[30vh] min-w-[30vw] text-white">
+    <div
+      className="relative min-h-[30vh] min-w-[30vw] text-white "
+      onMouseEnter={() => (isHoverImage.current = true)}
+      onMouseLeave={() => (isHoverImage.current = false)}
+    >
       <div className="relative h-56 overflow-hidden rounded-lg  md:h-96">
         {props.images.map((image, index) => (
           <div
             key={`${index + image}-img`}
-            className={`${onAimatedCondition(index)} absolute  top-0 h-full w-full transform transition duration-700
-            ease-in-out`}
+            className={`${onAimatedCondition(index)} absolute  top-0 h-full w-full transition duration-700 ease-in-out`}
           >
             <img
-              className=" absolute top-1/2 left-1/2 block w-full -translate-x-1/2 -translate-y-1/2 transform "
+              className=" absolute top-1/2 left-1/2 block w-full -translate-x-1/2 -translate-y-1/2 "
               alt="..."
               src={image}
             />
           </div>
         ))}
       </div>
-      {currentIndex}
+
+      <div className="absolute bottom-5 left-1/2 z-30 flex -translate-x-1/2 space-x-3">
+        {props.images.map((image, index) => (
+          <button
+            key={`${index}-slider`}
+            type="button"
+            className={`h-3 w-3 rounded-full  ${index !== currentIndexRef.current ? 'bg-white/30' : 'bg-white'} `}
+            aria-current="true"
+            aria-label="Slide 1"
+            data-carousel-slide-to="0"
+          ></button>
+        ))}
+      </div>
+
       <button
         onClick={onPrev}
         type="button"
