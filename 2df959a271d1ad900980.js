1b68e0bcf9a007bc54222feb6712090f8498ebf6
outsider987 <t7902195204@gmail.com>
2023-01-15 17:19:30 +0000

    Deploying to gh-pages from  @ 1fdb37765fc4b95a14d29aa01e9be55e28245257 🚀

--- 2df959a271d1ad900980.js
+++ 2df959a271d1ad900980.js
@@ -28,7 +28,7 @@ const Canvas = (props) => {
     let totalNumber = results.reduce((sum, { total }) => sum + total, 0);
     let lastValue = 0;
     const datas = results.map((result) => {
-        const lastPercentage = Math.ceil((lastValue / totalNumber) * 100);
+        const lastPercentage = (lastValue / totalNumber) * 100;
         const accumlatePercentage = (0,_utils_canvas__WEBPACK_IMPORTED_MODULE_1__.accumlateOfPercentange)(result.total, lastPercentage, totalNumber);
         lastValue += result.total;
         return {
@@ -48,6 +48,7 @@ const Canvas = (props) => {
             // ctx.fillStyle = 'white';
             // ctx.fillRect(0, 0, canvas.width, canvas.height);
             let endingPct = 100;
+            let radius = canvasRef.current.clientWidth / 4;
             let pct = 0;
             let cx = Math.ceil(cw / 2);
             let cy = Math.ceil(ch / 2);
@@ -72,13 +73,20 @@ const Canvas = (props) => {
                         if (moodValue.color === '#0a9627') {
                             // debugger;
                         }
-                        (0,_utils_canvas__WEBPACK_IMPORTED_MODULE_1__.drawPie)(ctx, { startAngle: moodValue.startAngle, endAngle: endRadians, cx, cy, color: moodValue.color });
+                        (0,_utils_canvas__WEBPACK_IMPORTED_MODULE_1__.drawPie)(ctx, {
+                            radius,
+                            startAngle: moodValue.startAngle,
+                            endAngle: endRadians,
+                            cx,
+                            cy,
+                            color: moodValue.color,
+                        });
                         (0,_utils_canvas__WEBPACK_IMPORTED_MODULE_1__.drawSegmentLabel)(ctx, {
                             cx,
                             cy,
                             startAngle: moodValue.startAngle,
                             endAngle: (0,_utils_canvas__WEBPACK_IMPORTED_MODULE_1__.getAngleOfPercentage)(moodValue.accumlatePercentage),
-                            radius: endingPct,
+                            radius,
                             percentage: moodValue.accumlatePercentage,
                             text: String(moodValue.percentage),
                         });
@@ -138,25 +146,29 @@ function drawSegmentLabel(ctx, dto) {
     ctx.textAlign = 'center';
     ctx.fillStyle = 'white';
     let theta = (dto.startAngle + dto.endAngle) / 2;
-    let deltaY = Math.ceil(Math.sin(theta) * centerDistance * 100);
-    let deltaX = Math.ceil(Math.cos(theta) * centerDistance * 100);
+    let deltaY = Math.ceil(Math.sin(theta) * centerDistance * dto.radius);
+    let deltaX = Math.ceil(Math.cos(theta) * centerDistance * dto.radius);
     ctx.fillText(dto.text, deltaX + dto.cx, deltaY + dto.cy);
     ctx.closePath();
 }
 function drawPie(ctx, dto) {
+    const strokeWidth = 2;
     ctx.beginPath();
-    ctx.arc(dto.cx, dto.cy, 100, dto.startAngle, dto.endAngle);
-    // ctx.stroke();
+    ctx.arc(dto.cx, dto.cy, dto.radius, dto.startAngle, dto.endAngle);
+    ctx.lineWidth = strokeWidth;
     ctx.lineTo(dto.cx, dto.cy);
+    ctx.closePath();
     ctx.save();
     ctx.clip();
     ctx.fillStyle = dto.color;
     ctx.fill();
+    ctx.lineWidth = strokeWidth;
+    ctx.strokeStyle = 'black';
+    ctx.stroke();
     ctx.restore();
-    ctx.closePath();
 }
 const getAngleOfPercentage = (percentage) => {
-    return Math.ceil((-Math.PI / 2 + (Math.PI * 2 * percentage) / 100) * 100) / 100;
+    return -Math.PI / 2 + (Math.PI * 2 * percentage) / 100;
 };
 const accumlateOfPercentange = (data, percentage, totalNumber) => {
     return Math.ceil(percentage + (data / totalNumber) * 100);
@@ -176,4 +188,4 @@ module.exports = __webpack_require__.p + "src/assets/img/me.af2f25858a0ff2c5b2b6
 /***/ })
 
 }]);
-//# sourceMappingURL=js/d000cb0d8cc38ed59ebb.js.map
+//# sourceMappingURL=js/2df959a271d1ad900980.js.map
