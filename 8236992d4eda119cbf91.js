7a5caa33c31583bf67f2de927310acde72bb6921
outsider987 <t7902195204@gmail.com>
2023-02-11 08:02:11 +0000

    Deploying to gh-pages from  @ 4c7854734c8e87bcca6504b4aa6260d03d8a1ed9 🚀

--- 8236992d4eda119cbf91.js
+++ 8236992d4eda119cbf91.js
@@ -19,13 +19,19 @@ const Carousel = (props) => {
     const currentIndexRef = (0,react__WEBPACK_IMPORTED_MODULE_0__.useRef)(0);
     const isHoverImage = (0,react__WEBPACK_IMPORTED_MODULE_0__.useRef)(false);
     const ZindexRef = (0,react__WEBPACK_IMPORTED_MODULE_0__.useRef)('z-[3]');
-    const onNext = () => {
+    const [startX, setStartX] = (0,react__WEBPACK_IMPORTED_MODULE_0__.useState)(0);
+    const [endX, setEndX] = (0,react__WEBPACK_IMPORTED_MODULE_0__.useState)(0);
+    const onNext = (e) => {
+        if (e)
+            e.preventDefault();
         const nextIndex = currentIndexRef.current === props.images.length - 1 ? 0 : currentIndexRef.current + 1;
         currentIndexRef.current = nextIndex;
         setCurrentIndex(currentIndexRef.current);
         ZindexRef.current = 'z-[3]';
     };
-    const onPrev = () => {
+    const onPrev = (e) => {
+        if (e)
+            e.preventDefault();
         const nextIndex = currentIndexRef.current === 0 ? props.images.length - 1 : currentIndexRef.current - 1;
         currentIndexRef.current = nextIndex;
         setCurrentIndex(currentIndexRef.current);
@@ -38,20 +44,42 @@ const Carousel = (props) => {
             return 'transform  translate-x-full z-[1] ';
         return `transform  -translate-x-full ${ZindexRef.current}`;
     };
+    const handleTouchStart = (e) => {
+        isHoverImage.current = true;
+        setStartX(e.touches[0].clientX);
+    };
+    const handleTouchMove = (e) => {
+        setEndX(e.touches[0].clientX);
+    };
+    const handleTouchEnd = (e) => {
+        isHoverImage.current = false;
+        const translateX = endX - startX;
+        if (endX === 0)
+            return;
+        if (translateX > 0) {
+            onPrev(e);
+        }
+        else {
+            onNext(e);
+        }
+    };
     (0,react__WEBPACK_IMPORTED_MODULE_0__.useEffect)(() => {
         const interval = setInterval(() => {
             !isHoverImage.current && onNext();
         }, 2800);
         return () => clearInterval(interval);
     }, []);
-    return (react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", { className: "relative min-h-[30vh] min-w-[30vw] text-white ", onMouseEnter: () => (isHoverImage.current = true), onMouseLeave: () => (isHoverImage.current = false) },
-        react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", { className: "relative h-56 overflow-hidden rounded-lg  md:h-96" }, props.images.map((image, index) => (react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", { key: `${index + image}-img`, className: `${onAimatedCondition(index)} absolute  top-0 h-full w-full transition duration-700 ease-in-out` },
-            react__WEBPACK_IMPORTED_MODULE_0___default().createElement("img", { className: " absolute top-1/2 left-1/2 block w-full -translate-x-1/2 -translate-y-1/2 ", alt: "...", src: image }))))),
-        react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", { className: "absolute bottom-5 left-1/2 z-30 flex -translate-x-1/2 space-x-3" }, props.images.map((image, index) => (react__WEBPACK_IMPORTED_MODULE_0___default().createElement("button", { onClick: () => {
+    const BottomItems = () => {
+        return (react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", { className: "absolute bottom-5 left-1/2 z-30 flex -translate-x-1/2 space-x-3" }, props.images.map((image, index) => (react__WEBPACK_IMPORTED_MODULE_0___default().createElement("button", { onClick: () => {
                 currentIndexRef.current < index ? (ZindexRef.current = 'z-[3]') : (ZindexRef.current = 'z-[0]');
                 currentIndexRef.current = index;
                 setCurrentIndex(currentIndexRef.current);
-            }, key: `${index}-slider`, type: "button", className: `h-3 w-3 rounded-full  ${index !== currentIndexRef.current ? 'bg-white/30' : 'bg-white'} `, "aria-current": "true", "aria-label": "Slide 1", "data-carousel-slide-to": "0" })))),
+            }, key: `${index}-slider`, type: "button", className: `h-3 w-3 rounded-full  ${index !== currentIndexRef.current ? 'bg-white/30' : 'bg-white'} `, "aria-current": "true", "aria-label": "Slide 1", "data-carousel-slide-to": "0" })))));
+    };
+    return (react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", { className: "relative min-h-[30vh] min-w-[30vw] text-white ", onMouseEnter: () => (isHoverImage.current = true), onMouseLeave: () => (isHoverImage.current = false), onTouchStart: handleTouchStart, onTouchMove: handleTouchMove, onTouchEnd: handleTouchEnd },
+        react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", { className: "relative h-56 overflow-hidden rounded-lg  md:h-96" }, props.images.map((image, index) => (react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", { key: `${index + image}-img`, className: `${onAimatedCondition(index)} absolute  top-0 h-full w-full transition duration-700 ease-in-out` },
+            react__WEBPACK_IMPORTED_MODULE_0___default().createElement("img", { className: " absolute top-1/2 left-1/2 block w-full -translate-x-1/2 -translate-y-1/2 ", alt: "...", src: image }))))),
+        react__WEBPACK_IMPORTED_MODULE_0___default().createElement(BottomItems, null),
         react__WEBPACK_IMPORTED_MODULE_0___default().createElement("button", { onClick: onPrev, type: "button", className: "group absolute top-0 left-0 z-20  flex h-full cursor-pointer items-center justify-center px-4 focus:outline-none", "data-carousel-prev": true },
             react__WEBPACK_IMPORTED_MODULE_0___default().createElement("span", { className: "inline-flex h-10 w-10 items-center justify-center rounded-full bg-white/30 text-orange-500 group-hover:bg-white/50 group-focus:outline-none group-focus:ring-4 group-focus:ring-white dark:bg-gray-800/30 dark:group-hover:bg-gray-800/60 dark:group-focus:ring-gray-800/70" },
                 react__WEBPACK_IMPORTED_MODULE_0___default().createElement("svg", { "aria-hidden": "true", className: "h-6 w-6 ", fill: "none", stroke: "currentColor", viewBox: "0 0 24 24", xmlns: "http://www.w3.org/2000/svg" },
@@ -99,4 +127,4 @@ const CarouselPage = () => {
 /***/ })
 
 }]);
-//# sourceMappingURL=js/a26178cd7b04c55a90d0.js.map
+//# sourceMappingURL=js/8236992d4eda119cbf91.js.map
